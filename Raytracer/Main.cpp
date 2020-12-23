5eb24566d0c7c5f2746e562232095d1f32ae001a
Zeshan Rasul <zeshanrasul@gmail.com>
2020-12-23 22:11:00 +0000

    FreeImage Setup

--- Raytracer/Main.cpp
+++ Raytracer/Main.cpp
@@ -1,4 +1,15 @@
+#include "FreeImage.h"
+#include <string>
+
 int main()
 {
+	const int width = 128;
+	const int height = 128;
+	unsigned char pixels[width * height * 3] = { 0 };
+	std::string outputFilename = "Raytracer.png";
+	
+	FreeImage_Initialise();
+	FIBITMAP* img = FreeImage_ConvertFromRawBits(pixels, width, height, 3 * width, 24, 0xFF0000, 0x00FF00, 0x0000FF, false);
+	FreeImage_Save(FIF_PNG, img, outputFilename.c_str(), 0);
 }
 
