385089105effbd079140b54cf272fa83ffec54ca
Zeshan Rasul <zeshanrasul@gmail.com>
2020-12-24 01:28:43 +0000

    Sphere class

---
+++ Raytracer/Sphere.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "glm/glm.hpp"
+
+using namespace glm;
+
+class Sphere
+{
+public:
+	Sphere(vec3 center, float radius, vec3 diffuse, vec3 specular, vec3 emission, float shininess)
+		:
+		center(center),
+		radius(radius),
+		diffuse(diffuse),
+		specular(specular),
+		emission(emission),
+		shininess(shininess)
+	{
+	}
+
+	vec3 center;
+	float radius;
+	vec3 diffuse, specular, emission;
+	float shininess;
+};
