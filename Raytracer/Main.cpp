419c3bc2ee46454250490b11239106b37abe70c9
Zeshan Rasul <zeshanrasul@gmail.com>
2020-12-25 00:52:05 +0000

    Lighting Improved

--- Raytracer/Main.cpp
+++ Raytracer/Main.cpp
@@ -250,7 +250,7 @@ Intersection FindIntersection(Scene scene, Ray ray)
 Ray ShootShadowRay(Intersection intersection, Light light)
 {
 
-	vec3 direction = intersection.intersectionPoint - light.position;
+	vec3 direction =  light.position - intersection.intersectionPoint;
 	vec3 origin = intersection.intersectionPoint + (direction * (0.00001f));
 
 	Ray ray(origin, direction);
@@ -260,12 +260,13 @@ Ray ShootShadowRay(Intersection intersection, Light light)
 vec3 ComputeLighting(Intersection intersection, Light light, Camera camera)
 {
 	vec3 finalColour;
-
-	float nDotL = dot(intersection.hitObjectNormal, light.position);
+	vec3 eyeDirection = normalize(camera.eyePos - intersection.intersectionPoint);
+	vec3 normalizedLightDirection = normalize(light.position);
+	float nDotL = dot(intersection.hitObjectNormal, normalizedLightDirection);
 	// No attenuation for now
 	vec3 lambert = intersection.hitObjectDiffuse * light.colour * max(nDotL, 0.0f);
 
-	vec3 halfVec = normalize(light.position + camera.eyePos);
+	vec3 halfVec = normalize(light.position + eyeDirection);
 	float nDotH = dot(intersection.hitObjectNormal, halfVec);
 	vec3 phong = intersection.hitObjectSpecular * light.colour * pow(max(nDotH, 0.0f), intersection.hitObjectShininess);
 
@@ -304,7 +305,7 @@ int main()
 	unsigned char pixels[width * height * 3] = { 0 };
 	std::string outputFilename = "Raytracer.png";
 
-	vec3 eyePosition = vec3(0, 2, -2);
+	vec3 eyePosition = vec3(0, 0, -1);
 	vec3 center = vec3(0, 0, 0);
 	vec3 up = vec3(0, 1, 0);
 	float fovY = radians(45.0f);
@@ -314,23 +315,23 @@ int main()
 	// Create new Scene and add Sphere and then Triangle
 	Scene scene;
 
-	Sphere sphere0(vec3(0, 0, 0), 0.15f, vec3(0.67, 0.33, 0.93), vec3(0.2, 0.2, 0.2), vec3(0.1, 0.1, 0.1), 20.0f, vec3(0.67, 0.33, 0.93));
+	Sphere sphere0(vec3(0, 0, 0), 0.15f, vec3(0.67, 0.33, 0.93), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, 0.0f), 0.0f, vec3(0.1, 0.1, 0.1));
 	scene.spheres.push_back(sphere0);
 
-	Sphere sphere1(vec3(0.5, 0.5, 0.75f), 0.5f, vec3(0.67, 0.33, 0.93), vec3(0.2, 0.2, 0.2), vec3(0.1, 0.1, 0.1), 20.0f, vec3(0.67, 0.33, 0.93));
+	Sphere sphere1(vec3(0.5, 0.5, 0.15f), 0.5f, vec3(0.67, 0.33, 0.93), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, 0.0f), 0.0f, vec3(0.1, 0.1, 0.1));
 	scene.spheres.push_back(sphere1);
 
-	Triangle triangle0(vec3(-0.33, 0.33, 1), vec3(0.33, -0.33, 1), vec3(0.33, 0.33, 1), vec3(0.0f, 0.27f, 0.619f), vec3(0.2, 0.2, 0.2), vec3(0.1, 0.1, 0.1), 20.0f, vec3(0.0f, 0.33f, 0.33f));
+	Triangle triangle0(vec3(-0.33, 0.33, 1.0f), vec3(0.33, -0.33, 1), vec3(0.33, 0.33, 1), vec3(0.619f, 0.27f, 0.619f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, 0.0f), 0.0f, vec3(0.1, 0.1, 0.1));
 	scene.triangles.push_back(triangle0);
 
-	Triangle triangle1(vec3(+0.33, -0.33, 1), vec3(-0.33, 0.33, 1), vec3(-0.33, -0.33, 1), vec3(0.0f, 0.27f, 0.619f), vec3(0.2, 0.2, 0.2), vec3(0.1, 0.1, 0.1), 20.0f, vec3(0.0f, 0.33f, 0.33f));
+	Triangle triangle1(vec3(+0.33, -0.33, 1.0f), vec3(-0.33, 0.33, 1), vec3(-0.33, -0.33, 1), vec3(0.619f, 0.27f, 0.619f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, 0.0f), 0.0f, vec3(0.1, 0.1, 0.1));
 	scene.triangles.push_back(triangle1);
 
 	Light light0(vec3(4, 0, 4), vec3(0.5f, 0.5f, 0.5f));
 	scene.lights.push_back(light0);
 	
-	Light light1(vec3(0, 0, -1), vec3(0.0f, 0.5f, 0.7f));
-	scene.lights.push_back(light1);
+	//Light light1(vec3(0, 0, -1), vec3(0.0f, 0.5f, 0.7f));
+	//scene.lights.push_back(light1);
 
 	for (int i = 0; i < width; i++)
 	{
